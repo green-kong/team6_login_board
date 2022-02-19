9038cc51f217115c0bfa8dfd6a06a692ad41e1b9
dev_kong <ahdjd5@gmail.com>
2022-02-20 02:01:31 +0900

    feat: add slide to mainpage

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,46 @@
 const slideView = document.querySelector('#slide_view');
 const header = document.querySelector('#header');
 const topTxT = document.querySelector('#top_text');
+const slideImg = document.querySelector('.slide_img');
+const firstImg = document.querySelector('#first_img');
+const imgFrame = document.querySelector('#slide_img_list');
+
+const imgWidth = slideImg.getBoundingClientRect().width;
+
+let isSlide = false;
+let slideCount = 0;
+
+const firstImgClone = firstImg.cloneNode(true);
+
+imgFrame.appendChild(firstImgClone);
+
+const slide = () => {
+  slideCount += 1;
+  imgFrame.style.transform = `translateX(-${imgWidth * slideCount}px)`;
+
+  if (slideCount === 4) {
+    console.log(slideCount);
+    imgFrame.style.transform = `translateX(-${imgWidth * slideCount}px)`;
+    setTimeout(() => {
+      imgFrame.style.transition = 'none';
+      imgFrame.style.transform = 'translateX(0px)';
+    }, 350);
+    slideCount = 0;
+  }
+  imgFrame.style.transition = 'all 350ms ease-in';
+};
+
+let slideInterval = null;
+
+const imgSlide = () => {
+  if (!isSlide) {
+    console.log('check');
+    isSlide = !isSlide;
+    slideInterval = setInterval(slide, 2500);
+  }
+};
 
 window.addEventListener('scroll', () => {
-  console.log(window.scrollY);
   if (window.scrollY > 200) {
     slideView.style.width = `${60 + (window.scrollY - 200) / 10}vw`;
   }
@@ -22,8 +59,11 @@ window.addEventListener('scroll', () => {
 
   if (window.scrollY > 580) {
     topTxT.classList.add('fix');
+    imgSlide();
   } else {
     topTxT.classList.remove('fix');
+    clearInterval(slideInterval);
+    isSlide = false;
   }
 
   if (window.scrollY > 1280) {
