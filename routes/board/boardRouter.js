2b3d6f05cd4f0b9b7890a4a3a06b4037f8aedd7e
kangcodeis <miiren543@damu.net>
2022-02-22 17:24:31 +0900

    feat:[Back] board routes/board views

--- routes/board/boardRouter.js
+++ routes/board/boardRouter.js
@@ -1,48 +1,41 @@
 const express = require('express');
-const res = require('express/lib/response');
+// const res = require('express/lib/response');
 const router = express.Router();
 
-// app.get('/board/list',(req,res)=>{
-//     res.render('board/list.html');
-//   })
-  app.get('/board/list',(req,res) =>{
-    res.render('board_list.html',{
-      list:list
+  router.get('/board/list',(req,res) =>{
+    res.render('board_list.html',{    
     })
   })
   
-  app.get('/board/write',(req,res)=>{
+  router.get('/board/write',(req,res)=>{
     res.render('board/write.html');
   })
   
-  // app.post('/board/write',(req,res)=>{
-  //   res.send('board/view');
-  
-  // })
-  
-  app.post('/board/write',(req,res)=>{
-    let board={...req.body}
-    list.push(board)
+  router.post('/board/write',(req,res)=>{
+    let item={...req.body}
+    data.push(board)
     res.redirect('/board/list.html');
   })
-  app.get('/board/view',(req,res)=>{
-    res.render('board/view.html');
-  
+
+  router.get('/board/view',(req,res)=>{
+    const index = req.query.index
+    const view = list[index-1]
+    res.render('view',{
+      data:view,
+      index:index,
+    })
   })
   
-  app.get('/board/edit',(req,res)=>{
+  router.get('/board/edit',(req,res)=>{
     res.render('board/view.html');
-  
   })
   
-  app.post('/board/edit',(req,res)=>{
+  router.post('/board/edit',(req,res)=>{
       res.send('/board/view.html');
-  
   })
   
-  app.post('/board/delete',(req,res)=>{
-      res.send('/board/list.html');
-  
+  router.post('/board/delete',(req,res)=>{
+      res.send('/board/list.html'); 
   })
 
   module.exports = router;//라우터 보내는 파일
