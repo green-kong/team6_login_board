a9d7492759a945645aac6bca844e098d1eacccb6
jongs <jongnam0309@gmail.com>
2022-02-22 18:09:20 +0900

    feat : back_adminloginpage

--- routes/admin/adminRouter.js
+++ routes/admin/adminRouter.js
@@ -1,34 +1,87 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../../models/db/db.js');
 
-//http://localhost:3000/admin <- 로그인하기 전 메인페이지
+//http://localhost:3000/admin <- 로그인페이지
 //router.get('/admin')
-router.get('/admin', (req, res) => {
-    res.render('admin')
+router.get('/', (req, res) => {
+    res.render('admin/admin.html')
 })
 
-// router.post('/admin', (req, res) => {  // 로그인 하려면 admin계정 정보랑 일치하는지 확인 
+router.post('/', async (req, res) => {
+    const { userid, userpw } = req.body;
+    const conn = await pool.getConnection();
+    try {
+        const [result] = await conn.query(`SELECT * FROM user WHERE userid = "${userid}"`);
+        console.log(result[0].userpw);
+        console.log(result);
+        if (result[0].userpw === userpw) {
+            if (result[0].level !== 1) {
+                res.send(alertmove('/admin', '접근권한이 없습니다.'))
+            } else {
+                res.redirect('/')
+            }
+            req.session.admin = result[0] // admin 정보 가져오기위한 저장공간이 세션
+            res.redirect('/') // 로그인 성공, 메인페이지로 이동
+        } else {
+            res.send(alertmove('/admin'), '관리자 비밀번호가 일치하지 않습니다.')
+        }
+        res.send(result); // 
+        conn.release;
+    } catch (error) {
+        console.log(error);
+        conn.release;
+    }
+    res.send()
+})
+
+// router.post('/', async (req, res) => {  // 로그인 하려면 admin계정 정보랑 일치하는지 확인 
 //     const { userid, userpw } = req.body;
 //     const conn = await pool.getConnection();
 //     try {
-//         const sql = `SELECT * FROM user WHERE userid = admin`
+//         const conn = await pool.getConnection();
+//         const [result] = await conn.query(`SELECT * FROM user WHERE userid = admin`, (error, result) => {
+//             if (error) return console.log(error);
+//             if (result) {
+//                 console.log(result)
+//             }
+//         })
+//         res.send(result)
+//         conn.release();
+//         res.redirect('/')
+//     } catch (error) {
+//         console.log(error)
+//         conn.release();
+//         res.redirect('/')
 //     }
 // })
 
+router.get('/user', async (req, res) => {
+    const { userid, username, alias, email, tel, mobile, level, isActive } = req.body;
+    const conn = await pool.getConnection();
+    try {
+        const [result] = await conn.query(`SELECT * FROM user`);
+        res.send(result)
+        conn.release();
+    } catch (error) {
+        console.log(error)
+        conn.release();
+    }
+});    // userlist 목록만 보이는곳 
 
-router.get('/admin/user')    // userlist 목록만 보이는곳 
+
+router.get('/user/edit', async (req, res) => {
+
+})  // userlist목록에서 '수정'버튼을 누르면 회원정보 수정하는곳 
 
 
-router.get('/admin/user/edit')  // userlist목록에서 '수정'버튼을 누르면 회원정보 수정하는곳 
+router.post('/user/edit') // 수정내용 저장 후 어느 페이지로 갈지 내가 정하고  
 
 
-router.post('/admin/user/edit') // 수정내용 저장 후 어느 페이지로 갈지 내가 정하고  
+router.get('/board')  // 특정회원 게시글보는기능
 
 
-router.get('/admin/board')  // 특정회원 게시글보는기능
-
-
-router.post('/admin/board') // 그 특정회원 게시글 수정 삭제 권한 
+router.post('/board') // 그 특정회원 게시글 수정 삭제 권한 
 
 
 
