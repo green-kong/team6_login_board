29f296fd7f1dc66b3a3bbe318fd06e727216dc4e
ads1213@nate.com <ads1213@nate.com>
2022-02-22 10:57:52 +0900

    style: [Back] add user / join router

--- routes/user/userController.js
+++ routes/user/userController.js
@@ -9,33 +9,32 @@ exports.joincheck = async (req,res)=>{
     const { body } = req;
     const conn = await pool.getConnection();
     try {
-        const usertel = "${body.usertel1}-${body.usertel2}-${body.usertel3}"
-        const usermobile = "${body.usermobile1}-${body.usermobile2}-${body.usermobile3}"
+        const usertel = `${body.usertel1}-${body.usertel2}-${body.usertel3}`;
         const sql = `INSERT INTO user
                 (userid, userpw, username, alias, birthdate, email, gender, mobile, tel)
                 values
                 ("${body.userid}", "${body.userpw}", "${body.username}", "${body.useralias}", "${body.userBirthYear}-${body.userBirthMonth}-${body.userBirthDay}", "${body.useremail}", "${body.usergender}",
-                "${body.usermobile1}-${body.usermobile2}-${body.usermobile3}", "${body.usertel1}-${body.usertel2}-${body.usertel3}")`
+                "${body.usermobile1}-${body.usermobile2}-${body.usermobile3}", "${body.usertel1}-${body.usertel2}-${body.usertel3}")`;
         const sql2 = `INSERT INTO user
                 (userid, userpw, username, alias, birthdate, email, gender, mobile)
                 values
                 ("${body.userid}", "${body.userpw}", "${body.username}", "${body.useralias}", "${body.userBirthYear}-${body.userBirthMonth}-${body.userBirthDay}", "${body.useremail}", "${body.usergender}",
-                "${body.usermobile1}-${body.usermobile2}-${body.usermobile3}")`
-        if ( `${usertel}` == undefined ) {
+                "${body.usermobile1}-${body.usermobile2}-${body.usermobile3}")`;
+        if ( usertel == undefined ) {
             await conn.query(sql);
         } else {
             await conn.query(sql2);
-        }
+        };
     } catch (error){
         throw error;
     } finally {
         conn.release();
     }
-    res.redirect('/user/welcome');
+    res.send(alertmove('/','회원가입이 완료되었습니다.'));
 }
 
 exports.login = (req,res)=>{
-    res.send('hello world')
+    res.render('user/login.html')
 };
 
 exports.logincheck = (req,res)=>{
@@ -46,7 +45,7 @@ exports.logout = (req,res) => {
     req.session.destroy(() => {
         req.session;
     });
-    res.render(alertmove('/','로그아웃이 완료되었습니다. '));
+    res.send(alertmove('/','로그아웃이 완료되었습니다.'));
 }
 
 exports.profile = (req,res)=>{
@@ -59,5 +58,11 @@ exports.profilecheck = (req,res)=>{
 };
 
 exports.quit = (req,res)=>{
-    res.send('hello world');
+    const { user } = req.session;
+    res.send(alertmove('/','회원탈퇴가 완료되었습니다.'))
+};
+
+exports.welcome = (req,res)=>{
+    const { user } = req.session;
+    res.render('user/welcome.html');
 };
