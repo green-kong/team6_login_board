3360004268ea9c681cea18d7c705b9fc8aa1f1e3
kangcodeis <miiren543@damu.net>
2022-02-23 17:27:30 +0900

    [back] feat baord write post  router

--- routes/board/boardRouter.js
+++ routes/board/boardRouter.js
@@ -3,22 +3,40 @@ const pool = require('../../models/db/db.js');
 const router = express.Router();
 
 router.get('/list',async (req,res)=>{
+  const conn = await pool.getConnection();
   try{
-    const conn = await pool.getConnection();
     const [result] = await conn.query(`SELECT * FROM board`);
     console.log(result);
     res.render('board/list.html',{result});
 } catch(err) {
     console.log(error);
+}  finally{
+  conn.release()
 }
-   
+
 });
 
-router.get('/write',(req,res)=>{
+router.get('/write',(req,res)=>{ 
   res.render('board/write.html');
 });
 
+router.post('/write',async (req,res)=>{
+  const {subject,content} = req.body;
+  const author = req.session.user._id;  
+  const conn = await pool.getConnection();
+  try{
+    const [result] = await conn.query(`INSERT INTO board(subject,author,content,date) values('${subject}','${author}','${content}',curdate());`)
+    console.log(result)
+    res.redirect(`/board/view?_id=${result.insertId}`);
+} catch(error) {
+  console.log(error);
+} finally{
+  conn.release()
+}
+});
+
 router.get('/view',(req,res)=>{
+  
   res.render('board/view.html');
 });
 
