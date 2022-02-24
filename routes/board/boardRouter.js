236533a51e98e79f72881e162ec3e50356181904
dev_kong <ahdjd5@gmail.com>
2022-02-24 20:47:31 +0900

    [back]feat: board-list-page

--- routes/board/boardRouter.js
+++ routes/board/boardRouter.js
@@ -5,25 +5,40 @@ const router = express.Router();
 
 router.get('/list', async (req, res) => {
   const page = Number(req.query.page);
-  const conn = await pool.getConnection();
-  try {
-    const sql = `SELECT board._id, subject, date, hit, alias 
+  if (page < 1) {
+    res.send(alertmove('/board/list?page=1', '접근할 수 없는 페이지 입니다.'));
+  } else {
+    const conn = await pool.getConnection();
+    try {
+      const sql = `SELECT board._id, subject, date, hit, alias 
                   FROM board
                   JOIN user
                   ON board.author = user._id
                   LIMIT ${(page - 1) * 10},10 `;
-    const [result] = await conn.query(sql);
-    result.forEach((v, i) => {
-      const dateYear = v.date.getFullYear();
-      const dateMonth = v.date.getMonth();
-      const dateDay = v.date.getDate();
-      result[i].date = `${dateYear}-${dateMonth}-${dateDay}`;
-    });
-    res.render('board/list.html', { result, page });
-  } catch (error) {
-    console.log(error);
-  } finally {
-    conn.release();
+      const [result] = await conn.query(sql);
+      const lastPage = Math.ceil(result.length / 10) || 1;
+      if (page > lastPage) {
+        res.send(
+          alertmove(
+            `/board/list?page=${lastPage}`,
+            '게시물이 없는 페이지 입니다. 마지막 페이지로 이동합니다.'
+          )
+        );
+      } else {
+        result.forEach((v, i) => {
+          const dateYear = v.date.getFullYear();
+          const dateMonth = v.date.getMonth();
+          const dateDay = v.date.getDate();
+          result[i].date = `${dateYear}-${dateMonth}-${dateDay}`;
+        });
+        const pageCal = Math.ceil(page / 5);
+        res.render('board/list.html', { result, pageCal, page });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      conn.release();
+    }
   }
 });
 
