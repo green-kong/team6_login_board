a8b4570e0a04d0afc8e4bae1ab21940881746bc1
dev_kong <ahdjd5@gmail.com>
2022-02-24 17:57:14 +0900

    [front]fix: mainpage slied img position

--- public/js/index.js
+++ public/js/index.js
@@ -43,7 +43,9 @@ const imgSlide = () => {
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 200) {
+    console.log(window.scrollY);
     slideView.style.width = `${60 + (window.scrollY - 200) / 10}vw`;
+    // imgFrame.style.left = `${-20 + (window.scrollY - 400) / 10}vw`;
   }
 
   if (window.scrollY > 280) {
