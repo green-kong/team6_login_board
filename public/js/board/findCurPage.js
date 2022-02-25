b22cadd21f907f1fa63173396d9eeff4345ded0b
dev_kong <ahdjd5@gmail.com>
2022-02-26 00:47:00 +0900

    [back]feat: admin board user list pagenation

--- public/js/board/findCurPage.js
+++ public/js/board/findCurPage.js
@@ -3,6 +3,5 @@ const curPage = document.querySelector('#cur_page').innerHTML;
 
 (function findCurPage() {
   const index = curPage % 5 ? (curPage % 5) - 1 : 4;
-  console.log(index);
   pageList[index].classList.add('emphasis');
 })();
